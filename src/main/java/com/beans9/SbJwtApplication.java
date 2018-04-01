9e49bd54db9d2e3030eeb015521df66c0a2ce728
homin ahn <ahmax95@gmail.com>
2018-04-02 00:27:53 +0900

    jpa crud

---
+++ src/main/java/com/beans9/SbJwtApplication.java
@@ -0,0 +1,12 @@
+package com.beans9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJwtApplication.class, args);
+	}
+}
